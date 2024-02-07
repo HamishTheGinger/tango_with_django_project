54162720bfc7caa0695e2b2779b9a64ea6620e10
Hamish Allan <2792542A@student.gla.ac.uk>
2024-02-07 17:46:09 +0000

    FixChapter3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,13 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
-STATIC_DIR =  os.path.join(BASE_DIR, 'static')
-
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -33,9 +26,6 @@ SECRET_KEY = 't2i%9$k^@c%b51wu$g%klbbunq)5jq@$2jzak%v9h6lj7bj16%'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-LOGIN_URL = 'rango:login'
-
 
 
 # Application definition
@@ -65,7 +55,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,8 +63,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media', # Check/add this line!
-
             ],
         },
     },
@@ -131,12 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [STATIC_DIR, ]
-
-# Media Files
-
-MEDIA_ROOT = MEDIA_DIR
-
-MEDIA_URL = '/media/'
-
